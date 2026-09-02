1ff54a79478c5e2597ff95cc6b628b0cb022961b
weyn <kyryloulianenko@gmail.com>
2026-09-02 11:15:44 +0200

    added Dockerfile

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template
 import psutil
 import platform
@@ -127,4 +128,7 @@ def api_stats():
     return jsonify(get_stats())   # ← JSON
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
